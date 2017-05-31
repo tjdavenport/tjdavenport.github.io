4faffa57a1339869a01ba530414a5e39077ea69d
Torrance Davenport <tjdavenport2@gmail.com>
2017-05-30 20:00:50 -0400

    adding deps

---
+++ spawnit-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  errorNotify: true,
+  scripts: [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/underscore/underscore.js',
+    './node_modules/backbone/backbone.js',
+    './node_modules/backbone.radio/build/backbone.radio.js',
+  ],
+};
